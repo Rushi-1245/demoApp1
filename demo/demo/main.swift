f1116742734c6d2954b89fa34946f98d48644dd6
Rushi-1245 <117622729+Rushi-1245@users.noreply.github.com>
2023-04-19 11:34:03 +0530

    Add files via upload

---
+++ demo/demo/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  demo
+//
+//  Created by siddhatech on 17/04/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
